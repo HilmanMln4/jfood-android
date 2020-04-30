fc78788addce61354620a6192593ba9a29ad1f6a
HilmanMln4 <hilman.maulana71@ui.ac.id>
2020-04-30 09:18:05 +0700

    Modul 9 - TP

---
+++ app/src/main/java/hilman/mln/jfood_android/RegisterRequest.java
@@ -0,0 +1,28 @@
+package hilman.mln.jfood_android;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterRequest extends StringRequest {
+    private static final String URL = "http://192.168.43.236:8080/customer/register";
+    private Map<String, String> params;
+
+    public RegisterRequest(String name, String email, String password, Response.Listener<String> listener){
+        super(Method.POST, URL, listener, null);
+        params = new HashMap<>();
+        params.put("name", name);
+        params.put("email", email);
+        params.put("password", password);
+    }
+
+    @Override
+    protected Map<String, String> getParams() throws AuthFailureError
+    {
+        return params;
+    }
+
+}
